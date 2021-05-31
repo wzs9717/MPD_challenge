3dc0ee3c5e65aaa4ede1a89ade588d0a14463b79
zhisheng.wang <zhisheng.wang@iglooinsure.com>
2021-05-31 23:18:17 +0800

    _

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import numpy as np
-X = np.array([[1, 1], [2, 1], [3, 1.2], [4, 1], [5, 0.8], [6, 1]])
-from sklearn.decomposition import NMF
-model = NMF(n_components=3, init='random', random_state=0)
-W = model.fit_transform(X)
-H = model.components_
-X_new = np.array([[1, 0], [1, 6.1], [1, 0], [1, 4], [3.2, 1], [0, 4]])
-W_new = model.transform(X_new)
-print(W)
-print(H)
-print(W_new)
+from lightfm import LightFM
+from lightfm.datasets import fetch_movielens
+from lightfm.evaluation import precision_at_k
+
+# Load the MovieLens 100k dataset. Only five
+# star ratings are treated as positive.
+data = fetch_movielens(min_rating=5.0)
+
+# Instantiate and train the model
+model = LightFM(loss='warp')
+model.fit(data['train'], epochs=30, num_threads=2)
+
+# Evaluate the trained model
+test_precision = precision_at_k(model, data['test'], k=5).mean()
