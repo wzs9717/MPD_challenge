943668c6b835d4930d6649852fe649c9160e2eaa
zhisheng.wang <zhisheng.wang@iglooinsure.com>
2021-06-19 00:06:26 +0800

    finish model first

--- train.py
+++ train.py
@@ -4,15 +4,22 @@ import pandas as pd
 from model import TwoStageModel
 def main():
     # // load data
-    df
-    # // generate split
+    df_tracks=read_csv("./new_data/tracks.csv")
+    df_pl=read_csv("./new_data/play_list.csv")
+    df_pl_track_map=read_csv("./new_data/pl_track_map.csv")
 
-    # // get all latents
+    df_pl_test=read_csv("./new_data/play_list_test.csv")
+    df_pl_track_map_test=read_csv("./new_data/pl_track_map_test.csv")
 
-    # // WMF
+    train_map = None
+    val1_map = None
+    val1_pids = None
+    val2_map = None
+    val2_pids = None
 
-    # // SVD on album, artist and name
-
+    # // train first stage model
+    model=TwoStageModel()
+    model.train(df_pl,df_tracks,train_map,val1_map,val2_map,val1_pids,val2_pids)
     # // train second stage model
 
 if __name__ == "__main__":
