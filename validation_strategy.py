80e5a9fa0c2cdc1a2d7ca78aa8a02f7a2d829151
Joey-jjj <Li20180115@outlook.com>
2021-06-19 00:00:35 +0800

    complete validation
    complete validation

--- validation_strategy.py
+++ validation_strategy.py
@@ -7,21 +7,24 @@ import pandas as pd
 def validation():
     df_tracks = pd.read_csv('./new_data/tracks.csv')
     df_playlists = pd.read_csv('./new_data/play_list.csv') 
-    df_playlists_map= pd.read_csv('./new_data/pl_track_map_test.csv')
+    df_playlists_map= pd.read_csv('./new_data/pl_track_map.csv')
     df_test=pd.read_csv('./new_data/pl_test.csv')
     df_test_map=pd.read_csv('./new_data/pl_test_map_validation.csv')
-    
     
     num_tracks = df_playlists.groupby('num_tracks').pid.apply(np.array)
     # num_pl, pl_name_list
 # 5, [‘summer’,winter]
 # 10,[‘daoxiang’]
+    #a=df_test.num_tracks.value_counts().reset_index()
+
     
     validation_playlists = {}
     for i, j in df_test.num_tracks.value_counts().reset_index().values:
          #5,1000
     #10,1200
-        validation_playlists[i] = np.random.choice(num_tracks.loc[i], 2 * j, replace=False)#??
+        if i in num_tracks.index:
+          
+            validation_playlists[i] = np.random.choice(num_tracks.loc[i], 2*j, replace=True)#??
          #{5:[‘summer’,winter]
     # 10,[‘daoxiang’,‘daoxiang’]}
     
@@ -34,12 +37,12 @@ def validation():
         
         value_counts = df_test.query('num_samples==@i').num_tracks.value_counts()
         for j, k in  value_counts.reset_index().values:
-            
-            val1_playlist[i] += list(validation_playlists[j][:k])
-            validation_playlists[j] = validation_playlists[j][k:]
-            
-            val2_playlist[i] += list(validation_playlists[j][:k])
-            validation_playlists[j] = validation_playlists[j][k:]
+            if j in num_tracks.index:
+                val1_playlist[i] += list(validation_playlists[j][:k])
+                validation_playlists[j] = validation_playlists[j][k:]
+                
+                val2_playlist[i] += list(validation_playlists[j][:k])
+                validation_playlists[j] = validation_playlists[j][k:]
     val1_index = df_playlists_map.pid.isin(val1_playlist[0])
     val2_index = df_playlists_map.pid.isin(val2_playlist[0])
     for i in [1, 5, 10, 25, 100]:
@@ -56,7 +59,6 @@ def validation():
     
     train = pd.concat([train, df_test_map])
     train.to_csv('./new_data/train.csv',index = None)
-
     val1.to_csv('./new_data/val1.csv',index = None)
     val2.to_csv('./new_data/val2.csv', index = None)
     
